26e9c605e3da848b458d847aeef06ce7889046cb
Tarciso Loiola <tarcisoloiola@Tarcisos-MacBook-Pro.local>
2018-08-08 11:02:20 -0400

    First Commit

---
+++ src/components/GlobalComponents/DivProp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Div = props => {
+    return(
+        <div>
+            { this.props }
+        </div>
+    )
+}
---
+++ src/components/PageRouter/PageRouter.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import NavigationBar from '../NavigationBar/NavigationBar';
+import OurCulture from '../Pages/OurCulture/OurCulture';
+import ContactUs from '../Pages/ContactUs/ContactUs';
+import Blog from '../Pages/Blog/Blog';
+import Header from '../Header/Header';
+import Menu from '../Pages/Menu/Menu';
+import Home from '../Pages/Home/Home';
+import Admin from '../Pages/Admin/Admin';
+import Cakes from '../Pages/Cakes/Cakes';
+import Cupcakes from '../Pages/Cupcakes/Cupcakes';
+import Minis from '../Pages/Minis/Minis';
+import Postres from '../Pages/Postres/Postres';
+import Drinks from '../Pages/Drinks/Drinks';
+import Others from '../Pages/Others/Others';
+// import 'styles/css/index.css';
+// import  from '';
+
+class PageRouter extends Component{
+    render(){
+        return(
+            <div>
+                <Header />
+                <div className = 'lf-page-router-container'>
+                    <aside className = 'lf-page-router-aside-container'>
+                        <NavigationBar />
+                    </aside>
+                    <section className = 'lf-page-router-section-container'>
+                        <Route exact path = '/' component = { Home } />
+                        <Route path = '/menu' component = { Menu } />
+                            <Route path = '/menu/cakes' component = { Cakes }/>
+                            <Route path = '/menu/cupcakes' component = { Cupcakes }/>
+                            <Route path = '/menu/minis' component = { Minis }/>
+                            <Route path = '/menu/postres' component = { Postres }/>
+                            <Route path = '/menu/drinks' component = { Drinks }/>
+                            <Route path = '/menu/others' component = { Others }/>
+                        <Route path = '/blog' component = { Blog } />
+                        <Route path = '/our-culture' component = { OurCulture} />
+                            <Route path = '/our-culture/contactus' component = { ContactUs } />
+                        <Route path = '/admin' component = { Admin } />
+                   </section>
+                </div>
+            </div>
+            )
+    }
+}
+
+export default PageRouter;
